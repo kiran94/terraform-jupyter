3bb96acd04f83188fc3a5983bc848f0ace99c9c2
kiran94 <kiran_patel94@hotmail.com>
2024-07-09 08:12:23 +0100

    refactor(user-data): configure jupyter lab instance

--- script.sh
+++ script.sh
@@ -17,11 +17,20 @@ sudo mount /dev/sdb /data
 sudo chown -R ec2-user:ec2-user /data
 sudo echo "UUID=$(lsblk -nr -o UUID,MOUNTPOINT | grep "/data" | cut -d ' ' -f 1) /data xfs defaults,nofail 1 2" >>/etc/fstab
 
+# Install Python Dependencies
 pip3 install urllib3==1.26.6
 pip3 install jupyterlab
 
 # Configure Jupyter for AWS HTTP
-runuser -l ec2-user -c 'jupyter notebook --generate-config' &&
-    sed -i -e "s/#c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '"$(curl http://169.254.169.254/latest/meta-data/public-hostname)"'/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py &&
-    sed -i -e "s/#c.NotebookApp.allow_origin = ''/c.NotebookApp.allow_origin = '*'/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py &&
-    sed -i -e "s/#c.NotebookApp.open_browser = True/c.NotebookApp.open_browser = False/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py
+runuser -l ec2-user -c 'jupyter lab --generate-config'
+
+CONFIG_FILE="/home/ec2-user/.jupyter/jupyter_lab_config.py"
+sudo chown -R ec2-user:ec2-user $CONFIG_FILE
+
+IP=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+
+sed -i "s/# c.ServerApp.ip = 'localhost'/c.ServerApp.ip = '"${IP}"'/" $CONFIG_FILE
+sed -i "s/# c.ServerApp.allow_origin = ''/c.ServerApp.allow_origin = '*'/g" $CONFIG_FILE
+sed -i "s/# c.ServerApp.open_browser = False/c.ServerApp.open_browser = False/g" $CONFIG_FILE
+
+echo 'DONE'
