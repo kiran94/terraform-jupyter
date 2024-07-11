3bf8efd07844032f92531a4cadf7f1bd7146b3f4
kiran94 <kiran_patel94@hotmail.com>
2024-07-11 20:59:28 +0100

    refactor(user-data): factor out local user

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# sleep 1m
+LOCAL_USER=ec2-user
+
+echo "export PS1='\t \u@\h '" >> .bashrc
 
 # Log stdout to file
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ec2-user/terraform.log 2>&1
+exec 1>/home/$LOCAL_USER/terraform.log 2>&1
 
 # Install Tools
 sudo yum update -y
@@ -15,7 +17,7 @@ sudo yum install vim git tmux jq -y
 sudo mkfs.xfs /dev/sdb -f
 sudo mkdir /data
 sudo mount /dev/sdb /data
-sudo chown -R ec2-user:ec2-user /data
+sudo chown -R $LOCAL_USER:$LOCAL_USER /data
 sudo echo "UUID=$(lsblk -nr -o UUID,MOUNTPOINT | grep "/data" | cut -d ' ' -f 1) /data xfs defaults,nofail 1 2" >>/etc/fstab
 
 # Install Python Dependencies
@@ -23,10 +25,10 @@ pip3 install urllib3==1.26.6
 pip3 install jupyterlab
 
 # Configure Jupyter for AWS HTTP
-runuser -l ec2-user -c 'jupyter lab --generate-config'
+runuser -l $LOCAL_USER -c 'jupyter lab --generate-config'
 
-CONFIG_FILE="/home/ec2-user/.jupyter/jupyter_lab_config.py"
-sudo chown -R ec2-user:ec2-user $CONFIG_FILE
+CONFIG_FILE="/home/$LOCAL_USER/.jupyter/jupyter_lab_config.py"
+sudo chown -R $LOCAL_USER:$LOCAL_USER $CONFIG_FILE
 
 IP=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
@@ -39,9 +41,9 @@ echo "c.Application.log_level = 'WARN'" >>$CONFIG_FILE
 echo "c.ServerApp.log_level = 'WARN'" >>$CONFIG_FILE
 
 # Configure Jupyter Overrides
-JUPYTERLAB_SETTINGS_DIR=/home/ec2-user/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/
+JUPYTERLAB_SETTINGS_DIR=/home/$LOCAL_USER/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/
 
-runuser -l ec2-user -c "mkdir -p ${JUPYTERLAB_SETTINGS_DIR}"
+runuser -l $LOCAL_USER -c "mkdir -p ${JUPYTERLAB_SETTINGS_DIR}"
 
 cat <<EOF >$JUPYTERLAB_SETTINGS_DIR/themes.jupyterlab-settings
 {
@@ -52,13 +54,13 @@ EOF
 # Install Pyenv
 sudo yum install gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel -y
 
-runuser -l ec2-user -c 'curl https://pyenv.run | bash'
+runuser -l $LOCAL_USER -c 'curl https://pyenv.run | bash'
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"
 [[ -d "$PYENV_ROOT/bin" ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"' >> /home/ec2-user/.bashrc
+eval "$(pyenv virtualenv-init -)"' >> /home/$LOCAL_USER/.bashrc
 
-echo '. ~/.bashrc' >> /home/ec2-user/.bash_profile
+echo '. ~/.bashrc' >> /home/$LOCAL_USER/.bash_profile
 
 echo 'DONE'
