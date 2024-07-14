7c63e77056a1aaebcb1465fd7f1d968d03450f99
kiran94 <kiran_patel94@hotmail.com>
2024-07-14 10:59:46 +0100

    chore(vars): add descriptions

--- vars.tf
+++ vars.tf
@@ -1,23 +1,28 @@
 variable "aws_region" {
-  type    = string
-  default = "eu-west-2"
+  type        = string
+  description = "The AWS region to deploy resources into"
+  default     = "eu-west-2"
 }
 variable "availability_zone" {
-  type    = string
-  default = "eu-west-2b"
+  type        = string
+  description = "The AWS Availability Zone to deploy resources into"
+  default     = "eu-west-2b"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+  type        = string
+  description = "The EC2 instance type to use for the Jupyter server"
+  default     = "t2.micro"
 }
 
 variable "ebs_volume_size_gb" {
-  type    = number
-  default = 8
+  type        = number
+  description = "The size of the EBS volume in GB"
+  default     = 8
 }
 
 variable "service" {
-  type    = string
-  default = "jupyter"
+  type        = string
+  description = "The name of the service to deploy. This is a arbitrary label."
+  default     = "jupyter"
 }
